3b9de44f6aa27ca6d26a33908e4a6456f44d9df0
chuyuwang <chuyuwang@ChuyuWangs-MacBook-Air.local>
2015-03-22 18:47:48 -0400

    table&schema
    几点不确定的问题
    1. html中<td data-bind="text: $schema"></td>
    2.<span data-bind="text: schemas().length"></span>
    请检查对不对
    table定义就是table，schema定义的是schema，其他变量是按照model写的

---
+++ build/web/javascripts/knockout_models/tableViewModel.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var restBaseUrl = "http://localhost:7654/";
+
+function tableViewModel(table) {
+	var self = this;
+
+	self.table = ko.observable(table);
+	
+}
+
+function tablesViewModel() {
+	var self = this;
+
+	self.tables = ko.observableArray();
+
+	self.newtable = ko.observable(new tableViewModel());
+
+	self.findAll = function() {
+		$.ajax({
+			url: restBaseUrl + "table",
+			type: 'GET',
+			dataType: 'json',
+			contentType: "application/json",
+			crossDomain: true,
+			success: function(data) {
+				self.tables.removeAll();
+
+				for (var i = 0; i < data.length; i++) {
+					var table = new tableViewModel(data[i].table);
+                   
+					self.tables.push(table);
+				}
+			},
+			error: function(data) {
+				alert("Something went wrong while getting dbs list. Please try again.");
+			}
+		});
+	};
+
+
+	self.findAll();
+}
+
+ko.applyBindings(new tableViewModel());
